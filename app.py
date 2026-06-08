3eca833dd9aed0db842ae389932c8125d3ee8530
Mohamed Sibaaq Ali Saleem <mohamedsibaaq@users.noreply.github.com>
2026-06-08 18:43:21 +0500

    Update app.run to use HOST environment variable for improved flexibility

--- app.py
+++ app.py
@@ -158,4 +158,5 @@ def static_files(path):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    host = os.environ.get("HOST", "127.0.0.1")
+    app.run(host=host, port=port)
